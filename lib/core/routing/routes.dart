b9d026daa19df59ccb31fda3cfd995e86ab56294
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-12-01 10:45:45 +0200

    - add routes

---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,7 @@
+class Routes {
+  static const String onBoarding = '/onboarding';
+  static const String home = '/home';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String training = '/training';
+}
