8db2cb3e2624ee49b185de10296f96ea5c7023f9
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-12-02 10:19:32 +0200

    - setup root and route

--- lib/fit_sphare_app.dart
+++ lib/fit_sphare_app.dart
@@ -1,17 +1,19 @@
+import 'package:fit_sphere/core/routing/routes.dart';
 import 'package:flutter/material.dart';
 
+import 'core/routing/app_router.dart';
+
 class FitSphareApp extends StatelessWidget {
-  const FitSphareApp({super.key});
+  final AppRouter appRouter;
+  const FitSphareApp({super.key, required this.appRouter});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text("FitSphere"),
-        ),
-      ),
+      title: 'FitSphere',
+      initialRoute:Routes.onBoarding,
+      onGenerateRoute: appRouter.onGenerateRoute,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:fit_sphere/fit_sphare_app.dart';
 import 'package:flutter/material.dart';
 
+import 'core/routing/app_router.dart';
+
 void main() {
-  runApp(const FitSphareApp());
+  runApp(FitSphareApp(
+    appRouter: AppRouter(),
+  ));
 }
 
