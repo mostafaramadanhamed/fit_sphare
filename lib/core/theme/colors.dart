0ec5fad7f35584a1c30734ef0c5c0a6b505b0b58
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-12-08 09:32:51 +0200

    - added text styles

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class ColorsManager {
   static const Color lightGreenYellow = Color(0xFFCEFF67);
   static const Color lightBlack = Color(0xFF252727);
+  static const Color grey = Color(0xFF7B7B7B);
 }
